db347ab200706ad336a999b1187ab4cade714d9d
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-10-27 22:26:42 +0530

    foreach

--- js/1.foreach.js
+++ js/1.foreach.js
@@ -1,6 +1,22 @@
-// foreach method
-const numbers = [1,2,3,4,5];
-// it is a callback function and it have 3 parameters 
-numbers.forEach((item,index,arr)=>{
-    console.log(item,index,arr);
+// // foreach method
+// const numbers = [1, 2, 3, 4, 5];
+// // it is a callback function and it have 3 parameters
+// numbers.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+// const numbers1 = [1, 2, 3, 4, 5];
+// numbers1.forEach((item, index, arr) => console.log(item, index, arr));
+
+const letter = ['a', 'b', 'c', 'd', 'a', 'b'];
+let count = {}
+letter.forEach((item,index,arr) => {
+    if (count[item]) {
+        count[item]++;
+        console.log(index,arr)
+    } else {
+        count[item] = 1;
+        console.log('else')
+    }
 })
+console.log(count)
