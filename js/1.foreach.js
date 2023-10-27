8ee9bf6055418d0ceda3cf4258042603f8e7a75c
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-10-27 20:08:31 +0530

    foreach

--- js/1.foreach.js
+++ js/1.foreach.js
@@ -2,5 +2,5 @@
 const numbers = [1,2,3,4,5];
 // it is a callback function and it have 3 parameters 
 numbers.forEach((item,index,arr)=>{
-    console.log(item*10,index--,arr+1);
+    console.log(item,index,arr);
 })
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Hey i wrote down the explanation of the Array Methods read and enjoy it..
+### [1.For Each Method](./markdown/foreach.md)
