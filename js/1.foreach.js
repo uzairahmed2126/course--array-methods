22750ba88670d689568a30c7fa79a309c32c35b0
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-10-27 18:02:48 +0530

    foreach

---
+++ js/1.foreach.js
@@ -0,0 +1,6 @@
+// foreach method
+const numbers = [1,2,3,4,5];
+// it is a callback function and it have 3 parameters 
+numbers.forEach((item,index,arr)=>{
+    console.log(item*10,index--,arr+1);
+})
