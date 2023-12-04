0a49091ad92868caceac7b5a40b30a9bf279b130
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-12-04 17:41:37 +0530

    foreach

--- js/1.foreach.js
+++ js/1.foreach.js
@@ -9,8 +9,17 @@
 // numbers1.forEach((item, index, arr) => console.log(item, index, arr));
 
 const letter = ['a', 'b', 'c', 'd', 'a', 'b'];
+const obj = {a:['a', 'b', 'c', 'd', 'a', 'b'],b:[1,2,3,4,5]}
 let count = {}
 letter.forEach((item, index, arr) => {
-    console.log(count[item] ? count[item]++ : count[item] = 1)
+    // count[item] ? count[item]++ : count[item] = 1
+    count[item] ? count[item]++ : count[item]=1;
 })
+console.log(count);
+
+
+obj.a.forEach(item => {
+    count[item] ? count[item]++ : count[item]=1;
+    // console.log(item);
+});
 console.log(count)
