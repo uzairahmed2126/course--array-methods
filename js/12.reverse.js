0c4b98d88a15f0509e359a7a314718b458d81a2b
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-20 23:08:19 +0530

    reverse

---
+++ js/12.reverse.js
@@ -0,0 +1,9 @@
+const items = [1, 2, 3];
+console.log(items); // [1, 2, 3]
+const reversedItems = items.reverse();
+console.log(reversedItems); // [3, 2, 1]
+console.log(items); // [1, 2, 3]
+
+const str = ['a','b','c'];
+str.reverse();
+console.log(str);
