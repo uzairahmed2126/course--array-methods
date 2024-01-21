a54bd5dfa3a8d00cfab163c7608736e812f8b0b2
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-21 19:43:14 +0530

    .;

--- js/15.unshift.js
+++ js/15.unshift.js
@@ -2,12 +2,3 @@ const str = ['c', 'd', 'e'];
 str.unshift('a', 'b');
 console.log(str);
 
-// without compare function
-const arr = [256, 64, 32, 16, 8, 4, 2];
-console.log(arr.sort()) // [16,  2, 256, 32,4, 64,   8]
-
-// with compare function
-arr.sort((a, b) => {
-    return a - b;
-});
-console.log(arr) // [2,  4,   8, 16,32, 64, 256]
--- js/7.sort.js
+++ js/7.sort.js
@@ -41,3 +41,13 @@ for (let i = 0; i < a.length; i++) {
     };
 };
 console.log(a)
+
+// without compare function
+const arr = [256, 64, 32, 16, 8, 4, 2];
+console.log(arr.sort()) // [16,  2, 256, 32,4, 64,   8]
+
+// with compare function
+arr.sort((a, b) => {
+    return a - b;
+});
+console.log(arr) // [2,  4,   8, 16,32, 64, 256]
