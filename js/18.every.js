f9619b4e51d11c6d01194326cedf9842b281443c
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-24 17:44:10 +0530

    every

--- js/18.every.js
+++ js/18.every.js
@@ -1,13 +1,23 @@
 const arr = [1, 2, 3, 4, 5];
-let result = arr.every(isLessThanZero);
-function isLessThanZero(item) {
-    return 0 < item;
-}
-console.log(result);
+// let result = arr.every(isLessThanZero);
+// function isLessThanZero(item) {
+//     return 0 < item;
+// }
+// console.log(result);
 
-const userNames = [{
-    firstName: 'Uzair',
-    lastName: 'Ahmed'
-}]
-let ans = userNames.every(function (person) { person.firstName == undefined });
-console.log(ans)
+// const userNames = [{
+//     firstName: 'Uzair',
+//     lastName: 'Ahmed'
+// }];
+// let ans = userNames.every(function (person) { person.firstName == undefined });
+// console.log(ans);
+
+let allPositive;
+for (let i = 0; i < arr.length; i++) {
+    const number = arr[i];
+    if (number > 0) {
+        allPositive = true;
+    }
+    allPositive = false;
+}
+console.log(allPositive);
