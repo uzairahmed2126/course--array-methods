ee046e51bdf99f18c3c74fc3d46d2308b28bcdbd
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-24 14:25:12 +0530

    foreach

--- js/1.foreach.js
+++ js/1.foreach.js
@@ -1,25 +1,37 @@
-// // foreach method
-// const numbers = [1, 2, 3, 4, 5];
-// // it is a callback function and it have 3 parameters
-// numbers.forEach((item, index, arr) => {
-//     console.log(item, index, arr);
+// // // foreach method
+// // const numbers = [1, 2, 3, 4, 5];
+// // // it is a callback function and it have 3 parameters
+// // numbers.forEach((item, index, arr) => {
+// //     console.log(item, index, arr);
+// // })
+
+// // const numbers1 = [1, 2, 3, 4, 5];
+// // numbers1.forEach((item, index, arr) => console.log(item, index, arr));
+
+// const letter = ['a', 'b', 'c', 'd', 'a', 'b'];
+// const obj = { a: ['a', 'b', 'c', 'd', 'a', 'b'], b: [1, 2, 3, 4, 5] }
+// let count = {}
+// letter.forEach((item, index, arr) => {
+//     // count[item] ? count[item]++ : count[item] = 1
+//     count[item] ? count[item]++ : count[item] = 1;
 // })
-
-// const numbers1 = [1, 2, 3, 4, 5];
-// numbers1.forEach((item, index, arr) => console.log(item, index, arr));
-
-const letter = ['a', 'b', 'c', 'd', 'a', 'b'];
-const obj = {a:['a', 'b', 'c', 'd', 'a', 'b'],b:[1,2,3,4,5]}
-let count = {}
-letter.forEach((item, index, arr) => {
-    // count[item] ? count[item]++ : count[item] = 1
-    count[item] ? count[item]++ : count[item]=1;
-})
-console.log(count);
+// console.log(count);
 
 
-obj.a.forEach(item => {
-    count[item] ? count[item]++ : count[item]=1;
-    // console.log(item);
-});
-console.log(count)
+// obj.a.forEach(item => {
+//     count[item] ? count[item]++ : count[item] = 1;
+//     // console.log(item);
+// });
+// console.log(count)
+
+const letters = ['a', 'b', 'c'];
+for (let index = 0; index < letters.length; index++) {
+    log(letters[index], index, letters);
+}
+
+function log(element, index, arr) {
+    console.log(index, element, arr);
+}
+
+
+
