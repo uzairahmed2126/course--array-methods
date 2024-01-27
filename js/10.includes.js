ced3b02757e459b607ca9b56559c33aba7e4793c
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-27 17:59:25 +0530

    includes

--- js/10.includes.js
+++ js/10.includes.js
@@ -3,7 +3,9 @@ const result = animals.includes('deer', 2);
 // console.log(result);
 
 
-const numbers= [1,2,3,4,5]
-const ans = numbers.includes(2,0)
-console.log(ans)
+const numbers = [1, 2, 3, 4, 5];
+const ans = numbers.includes(2, 0);
+console.log(ans);
 
+const str = 'abcdefghijklmnopqrstuvwxyz';
+console.log(str.includes('s', 1));
