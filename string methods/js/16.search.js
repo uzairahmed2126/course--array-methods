626a750c6993eac10a898950871d6b57eb4f4369
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-01-30 17:34:26 +0530

    search

--- string methods/js/16.search.js
+++ string methods/js/16.search.js
@@ -1,7 +1,12 @@
 const text = 'The rain in SPAIN stays mainly in the plain';
-console.log(text.search(/ain/));//5
-console.log(text.search('ain'));//5
-console.log(text.search(/AIn/));//-1
-console.log(text.search(/AIN/));//14
-console.log(text.search(/AIN/i));//5
-console.log(text.search(/aiN/i));//5
+// console.log(text.search(/ain/));//5
+// console.log(text.search('ain'));//5
+// console.log(text.search(/AIn/));//-1
+// console.log(text.search(/AIN/));//14
+// console.log(text.search(/AIN/i));//5
+// console.log(text.search(/aiN/i));//5
+const str = "hEy JudE";
+const re = /[A-Z]/;
+const reDot = /[.]/;
+console.log(str.search(re)); // returns 1, which is the index of the first capital letter "E"
+console.log(str.search(reDot)); // returns -1 cannot find '.' dot punctuation
