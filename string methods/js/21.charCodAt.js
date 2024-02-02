a21b7682f97cf0ee77e69584c58648eafb97a856
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-02-02 15:46:48 +0530

    charCodeAt

--- string methods/js/21.charCodAt.js
+++ string methods/js/21.charCodAt.js
@@ -3,3 +3,8 @@ console.log(str.charCodeAt(0));//32 for space
 
 const string = 'Hello World';
 console.log(string.charCodeAt(string.length - 1));
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+const index = 4;
+console.log(`Character code ${sentence.charCodeAt(index)} is equal to ${sentence.charAt(index)}`);
+// Expected output: "Character code 113 is equal to q"
